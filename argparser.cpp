cf9d8687242c97e5a74898a2a085cbd2bc75f7eb
N3kkii <vojta.ad22@gmail.com>
2024-09-25 23:49:47 +0200

    feat: basic implementation of ArgParser class

--- argparser.cpp
+++ argparser.cpp
@@ -1,52 +1,66 @@
 /**
  * @file argparser.cpp
  * @author Vojtěch Adámek
+ * 
  * @brief Definition of ArgParser class
  */
 
 #include "argparser.hpp"
 #include <vector>
 
+ArgParser::ArgParser(): port{143},
+                        mailbox{"INBOX"},
+                        only_new{false},
+                        only_headers{false},
+                        secured{false}
+{}
+
 void ArgParser::parse(char *argv[], int argc) {
-    // Convert C style array of const char* into a vector of strings
+    // Convert array of const char* into a vector of strings for easier comparison
     std::vector<std::string> args(argv, argv + argc);
 
     for (auto it = args.begin(); it != args.end(); it++) {
         if (*it == "-a") {
-
+            auth_file = *++it;
         }
         
         else if (*it == "-o") {
-
+            out_dir = *++it;
         }
 
         else if (*it == "-p") {
-
+            port = std::stoi(*++it);
         }
 
         else if (*it == "-n") {
-
+            only_new = true;
         }
 
         else if (*it == "-h") {
-
+            only_headers = true;
         }
 
         else if (*it == "-b") {
-
+            mailbox = *++it;
         }
 
         else if (*it == "-T") {
+            secured = true;
 
+            // change the default port for TLS
+            if (port == 143)
+                port = 993;
         }
 
         else if (*it == "-c") {
-
+            certfile = *++it;
         }
 
         else if (*it == "-C") {
-
+            certaddr = *++it;
         }
+
+        else server = *it;
     }
 
 }
--- argparser.hpp
+++ argparser.hpp
@@ -4,12 +4,14 @@
 /**
  * @file argparser.hpp
  * @author Vojtěch Adámek
+ * 
  * @brief Header file for ArgParser class
  * 
  * Class used for parsing command line arguments.
  * 
  * Parameters:
  *      Mandatory:
+ *          server
  *          -a auth_file
  *          -o out_dir
  * 
@@ -26,12 +28,47 @@
 
 #include <string>
 
+
 class ArgParser {
 public:
-    ArgParser() = default;
+    // Mandatory parameters
+    std::string server;
+    std::string auth_file;
+    std::string out_dir;
+    
+    // Optional parameters
+    int port;
+    std::string mailbox;
+    std::string certfile;
+    std::string certaddr;
+    bool only_new;
+    bool only_headers;
+    bool secured;
+
+
+    /**
+     * @brief Constructs ArgParser object
+     */
+    ArgParser();
+
+
+    /**
+     * @brief Default destructor
+     */
     ~ArgParser() = default;
 
+
+    /**
+    * @brief Parses the command line parameters
+    * 
+    * @param argv from main
+    * @param argc from main
+    * 
+    * @todo fix missing paremeter argument
+    */
     void parse(char *argv[], int argc);
+
+private:
 
 };
 
